18e48d7230bce69a8ae326a8d6bd86a276b135aa
martensonbj <martensonbj@gmail.com>
2016-01-09 16:55:07 -0700

    Edit and delete fully functional

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,7 @@ Rails.application.routes.draw do
 
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
-
   delete '/logout', to: "sessions#destroy"
 
   root 'sessions#new'
-
-
-
 end
