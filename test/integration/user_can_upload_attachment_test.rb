fccb0a6ff396e40f4b1cd8dd5680961f6ee0241c
martensonbj <martensonbj@gmail.com>
2016-01-09 13:41:31 -0700

    Start attachment functionality

--- test/integration/user_can_upload_attachment_test.rb
+++ test/integration/user_can_upload_attachment_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserCanUploadAttachmentTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user can attach an image to a post" do
+
+    visit new_user_post_path
+    fill_in "Title", with: "My First Post"
+    fill_in "Body", with: "This is a picture of something"
+    click_on SOMETHING
+
+  end
 end
