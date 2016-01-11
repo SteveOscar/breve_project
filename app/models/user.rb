cd3aff6035c20b152dc96a14d6b03ff7cab60f14
martensonbj <martensonbj@gmail.com>
2016-01-10 19:39:17 -0700

    Start recreating vanity url

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ActiveRecord::Base
   has_secure_password
 
   enum role: %w(default admin)
+
+  def generate_slug
+    slug = current_user.name
+  end
+
+  def to_params
+    slug.parameterize
+  end
+
 end
